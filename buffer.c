bfafda9d21c4df08e23fbae1f305c19984715f56
TSZEQ <98411500+TSZEQ@users.noreply.github.com>
2021-08-28 14:12:16 +0000

    Update player.html

--- buffer.c
+++ buffer.c
@@ -32,4 +32,4 @@ void
 buffer_shrink(struct buffer *buf)
 {
     memmove(buf->start, buf->pos, buf->last - buf->pos);
-    buf->last = buf->start + (buf
+    buf->last = buf->start + (buf->last - buf-
