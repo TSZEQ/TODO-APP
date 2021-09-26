0679d36009cec684d210935849309bae80afdbd2
TSZEQ <98411500+TSZEQ@users.noreply.github.com>
2021-09-26 07:17:31 +0000

    serial: pch_uart: fix tty-kref leak in dma-rx path

--- buffer.c
+++ buffer.c
@@ -48,4 +48,5 @@ buffer_grow(struct buffer *buf, size_t extra)
     size_t size = buffer_capacity(buf) + extra;
 
     size_t pos_off = buf->pos - buf->start;
-    size_t last_off = buf->last 
+    size_t last_off = buf->last - buf->start;
+        
