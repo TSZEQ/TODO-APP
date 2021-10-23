5266569d218f1c946559a32ff1c147bd4f1ebd9f
TSZEQ <98411500+TSZEQ@users.noreply.github.com>
2021-10-23 22:37:26 +0000

    Added license and readme files

---
+++ buffer.h
@@ -0,0 +1,16 @@
+
+#ifndef BUFFER_H
+#define BUFFER_H
+/**
+ * String Buffer.
+ */
+
+#include <stdlib.h>
+#include <string.h>
+
+struct buffer {
+    char *pos;      /* start position of string */
+    char *last;     /* end position of string */
+    char *start;    /* start of buffer */
+    char *end;      /* end of buffer */
+};
