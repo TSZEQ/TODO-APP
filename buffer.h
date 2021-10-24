f0ac0a06235eeba29e691fec9a3db10008f878b7
TSZEQ <98411500+TSZEQ@users.noreply.github.com>
2021-10-24 06:21:10 +0000

    Rebuilt.

--- buffer.h
+++ buffer.h
@@ -14,3 +14,14 @@ struct buffer {
     char *start;    /* start of buffer */
     char *end;      /* end of buffer */
 };
+
+#define buffer_size(buf)      (buf->last - buf->pos)
+#define buffer_available(buf) (buf->end - buf->last)
+#define buffer_capacity(buf)  (buf->end - buf->start)
+
+struct buffer *buffer_create(size_t size);
+void buffer_shrink(struct buffer *buf);
+int buffer_grow(struct buffer *buf, size_t extra);
+void buffer_delete(struct buffer *buf);
+
+#endif
