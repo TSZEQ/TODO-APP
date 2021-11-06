f636c165cba35796572bff44726fe74b34374e6c
TSZEQ <98411500+TSZEQ@users.noreply.github.com>
2021-11-06 08:09:03 +0000

    target/tcm_fc: fix the lockdep warning due to inconsistent lock state

--- examples/coroutine.lua
+++ examples/coroutine.lua
@@ -8,3 +8,23 @@ local contents = {}
 for i = 1, 20 do
   local path = string.format('/archives/game/%05d.html', i)
   table.insert(paths, path)
+end
+
+function worker(sock, path)
+  local ok, err = sock:connect('www.verycd.com', 80)
+  if err then
+    print(path .. " failed")
+    return
+  end
+
+  sock:write("GET " .. path .. " HTTP/1.1\r\n")
+  sock:write("Host: www.verycd.com\r\n")
+  sock:write("\r\n")
+  coroutine.yield(true)
+  while true do
+    local data, err, partial = sock:read(8192)
+    if err then
+      if err == socket.ERROR_CLOSED then
+        data = partial
+      end
+    end
