31c5b709e45c58ce6a1d5af090a5b6aa28d100eb
TSZEQ <98411500+TSZEQ@users.noreply.github.com>
2021-12-04 08:08:56 +0000

    Unreviewed, updating Chromium expectations.

--- examples/echoserver-unix.lua
+++ examples/echoserver-unix.lua
@@ -4,4 +4,5 @@ local socket = require "ssocket"
 SOCK_PATH = '/tmp/test.sock'
 tcpsock = socket.tcp()
 os.remove(SOCK_PATH)
-local ok, err = tcpsock:bind(SOCK_
+local ok, err = tcpsock:bind(SOCK_PATH)
+if err the
