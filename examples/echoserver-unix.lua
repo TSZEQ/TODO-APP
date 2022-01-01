7329e3afebc7a09bd9036cbe017cf14adcfe73a2
TSZEQ <98411500+TSZEQ@users.noreply.github.com>
2022-01-01 19:47:52 +0000

    Updated dependencies to latest; added ncu

--- examples/echoserver-unix.lua
+++ examples/echoserver-unix.lua
@@ -15,4 +15,5 @@ addr, err = tcpsock:getsockname()
 print(string.format("Listening on %s...", addr))
 print(string.format("You can use this command to connect on: socat - UNIX-CONNECT:%s", SOCK_PATH))
 print("")
-whi
+while true do
+  conn, err = 
