310eef8a46aae9caa890038965a55cb6b915131a
TSZEQ <98411500+TSZEQ@users.noreply.github.com>
2022-04-12 20:49:28 +0000

    Reviewer comments

---
+++ socket.c
@@ -0,0 +1,13 @@
+
+#include "compat.h"
+
+#include <lua.h>
+#include <lauxlib.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <errno.h>
+
+#include <unistd.h>
