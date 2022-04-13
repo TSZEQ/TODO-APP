dc71f6db37081a3e04bb22f150aea7aaf8fb3238
TSZEQ <98411500+TSZEQ@users.noreply.github.com>
2022-04-13 15:49:01 +0000

    untracked files

--- socket.c
+++ socket.c
@@ -11,3 +11,24 @@
 #include <errno.h>
 
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/un.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <netdb.h>
+#include <poll.h>
+#include <signal.h>
+#include "timeout.h"
+#include "buffer.h"
+
+#define _VERSION "0.0.1"
+
+#define TCPSOCK_TYPENAME     "TCPSOCKET*"
+#define UDPSOCK_TYPENAME     "UDPSOCKET*"
+
+/* Socket address */
+typedef union {
