9f01dd259e25af3e135a65a94b6c73305c4d7ccf
TSZEQ <98411500+TSZEQ@users.noreply.github.com>
2022-05-29 06:33:59 +0000

    ignore emacs backup files

--- t/Test/Builder/NoOutput.lua
+++ t/Test/Builder/NoOutput.lua
@@ -13,4 +13,5 @@ local m = {}
 function m.create ()
     local tb = require 'Test.Builder'.create()
     tb:output(io.tmpfile())
-    tb:failure_output(i
+    tb:failure_output(io.tmpfile())
+    tb:todo_out
