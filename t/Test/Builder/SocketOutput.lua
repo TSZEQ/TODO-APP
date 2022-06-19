cf6828db4ded9dfbb7148adee0439c4c498d4ede
TSZEQ <98411500+TSZEQ@users.noreply.github.com>
2022-06-19 12:44:19 +0000

    Fixes #33

--- t/Test/Builder/SocketOutput.lua
+++ t/Test/Builder/SocketOutput.lua
@@ -1,1 +1,20 @@
 
+
+--
+-- lua-TestMore : <http://fperrad.github.com/lua-TestMore/>
+--
+
+--[[
+    require 'Test.More'
+    require 'socket'
+    local conn = socket.connect(host, port)
+    require 'Test.Builder.SocketOutput'.init(conn)
+    -- now, as usual
+    plan(...)
+    ...
+--]]
+
+local assert = assert
+
+local tb = require 'Test.Builder'.new()
+local m = getmetatable(tb)
