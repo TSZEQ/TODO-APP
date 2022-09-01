6bed9b683a9e07a9f8fc1c7e708a2391e7e7850d
TSZEQ <98411500+TSZEQ@users.noreply.github.com>
2022-09-01 12:21:33 +0000

    can: sja1000: fix {pre,post}_irq() handling and IRQ handler return value

--- t/Test/Builder/Tester.lua
+++ t/Test/Builder/Tester.lua
@@ -100,4 +100,6 @@ function m.test_test (args)
     -- okay, reconnect the test suite back to the saved handles
     tb:output(original_output_handle)
     tb:failure_output(original_failure_handle)
-    tb:todo_output(original_
+    tb:todo_output(original_todo_handle)
+
+  
