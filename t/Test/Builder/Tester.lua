0a8ea2b79d517a7cc170023f8a292597a316eadb
TSZEQ <98411500+TSZEQ@users.noreply.github.com>
2022-09-01 05:38:24 +0000

    Better name

--- t/Test/Builder/Tester.lua
+++ t/Test/Builder/Tester.lua
@@ -99,4 +99,5 @@ function m.test_test (args)
 
     -- okay, reconnect the test suite back to the saved handles
     tb:output(original_output_handle)
-    tb:failure_output(original_failur
+    tb:failure_output(original_failure_handle)
+    tb:to
