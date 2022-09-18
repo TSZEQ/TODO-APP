31bf08b144674e6c39f6898372657ade59a42e60
TSZEQ <98411500+TSZEQ@users.noreply.github.com>
2022-09-18 10:29:55 +0000

    datetime

--- t/Test/Builder/Tester.lua
+++ t/Test/Builder/Tester.lua
@@ -135,4 +135,5 @@ return m
 --
 -- Copyright (c) 2009-2012 Francois Perrad
 --
--- This library is licensed under the terms of the MIT/X11 l
+-- This library is licensed under the terms of the MIT/X11 license,
+-- like Lua its
