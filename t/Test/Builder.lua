8e50bfffbf1c537cb6f377c6d617f826482db8cb
TSZEQ <98411500+TSZEQ@users.noreply.github.com>
2022-09-18 18:59:43 +0000

    dm: allocate buffer for messages with small number of arguments using GFP_NOIO

--- t/Test/Builder.lua
+++ t/Test/Builder.lua
@@ -9,3 +9,21 @@ local io = require 'io'
 local os = require 'os'
 local table = require 'table'
 local error = error
+local pairs = pairs
+local pcall = pcall
+local print = print
+local rawget = rawget
+local setmetatable = setmetatable
+local tonumber = tonumber
+local tostring = tostring
+local type = type
+
+_ENV = nil
+local m = {}
+
+local testout = io and io.stdout
+local testerr = io and (io.stderr or io.stdout)
+
+function m.puts (f, str)
+    f:write(str)
+end
