986a7a3db28b445feb714657b229ac60625d7c6c
TSZEQ <98411500+TSZEQ@users.noreply.github.com>
2022-09-25 22:42:22 +0000

    l10n: Updates to French (fr) translation

--- t/Test/Builder.lua
+++ t/Test/Builder.lua
@@ -262,3 +262,12 @@ function m:ok (test, name, level)
     self.curr_test = self.curr_test + 1
     name = tostring(name)
     if name:match('^[%d%s]+$') then
+        self:diag("    You named your test '" .. name .."'.  You shouldn't use numbers for your test names."
+        .. "\n    Very confusing.")
+    end
+    local out = ''
+    if not test then
+        out = "not "
+    end
+    out = out .. "ok " .. self.curr_test
+    if name ~= '' then
