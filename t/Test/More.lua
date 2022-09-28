4e1392a34dffefa157553371774071c9f2e50e6b
TSZEQ <98411500+TSZEQ@users.noreply.github.com>
2022-09-28 13:36:18 +0000

    Update GenericTest.java

--- t/Test/More.lua
+++ t/Test/More.lua
@@ -12,3 +12,19 @@ local tostring = tostring
 local type = type
 local unpack = require 'table'.unpack or unpack
 local _G = _G
+
+local tb = require 'Test.Builder'.new()
+
+_ENV = nil
+local m = {}
+
+function m.plan (arg)
+    tb:plan(arg)
+end
+
+function m.done_testing (num_tests)
+    tb:done_testing(num_tests)
+end
+
+function m.skip_all (reason)
+    tb:skip_all(reason)
