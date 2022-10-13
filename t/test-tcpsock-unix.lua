d610e422621dcd23fe923010b174a7f038e540b7
TSZEQ <98411500+TSZEQ@users.noreply.github.com>
2022-10-13 22:06:41 +0000

    BACKUP

--- t/test-tcpsock-unix.lua
+++ t/test-tcpsock-unix.lua
@@ -4,4 +4,5 @@ local filedir = filepath:match('(.+)/[^/]*') or '.'
 package.path = string.format(";%s/?.lua;%s/../?.lua;", filedir, filedir) .. package.path
 package.cpath = string.format(";%s/?.so;%s/../?.so;", filedir, filedir) .. package.cpath
 
-require '
+require 'Test.More'
+local soc
