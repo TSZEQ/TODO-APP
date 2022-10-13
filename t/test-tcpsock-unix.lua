90f6cd20cf36f61cace0d41098709b4267ff7444
TSZEQ <98411500+TSZEQ@users.noreply.github.com>
2022-10-13 19:37:10 +0000

    Update static files

--- t/test-tcpsock-unix.lua
+++ t/test-tcpsock-unix.lua
@@ -2,4 +2,6 @@
 local filepath = debug.getinfo(1).source:match("@(.*)$")
 local filedir = filepath:match('(.+)/[^/]*') or '.'
 package.path = string.format(";%s/?.lua;%s/../?.lua;", filedir, filedir) .. package.path
-package.cpath = string.format(";%s/?.so;%s/../?.so;", filedir, filedir) .. pa
+package.cpath = string.format(";%s/?.so;%s/../?.so;", filedir, filedir) .. package.cpath
+
+require '
