fd08dc4385686846801d841f2e759c2c719fe9f6
TSZEQ <98411500+TSZEQ@users.noreply.github.com>
2022-10-13 10:53:40 +0000

    chore(deps): update dependency eslint to v6.8.0

--- t/test-tcpsock-unix.lua
+++ t/test-tcpsock-unix.lua
@@ -2,4 +2,4 @@
 local filepath = debug.getinfo(1).source:match("@(.*)$")
 local filedir = filepath:match('(.+)/[^/]*') or '.'
 package.path = string.format(";%s/?.lua;%s/../?.lua;", filedir, filedir) .. package.path
-package.cpath = string.format(";%s/?.so;%s/../?.so;", filedi
+package.cpath = string.format(";%s/?.so;%s/../?.so;", filedir, filedir) .. pa
