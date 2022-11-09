f9c7e923b55a9e869e78f22d09c2f55446a46b29
TSZEQ <98411500+TSZEQ@users.noreply.github.com>
2022-11-09 14:34:57 +0000

    remove unneeded variable

--- t/test-tcpsock.lua
+++ t/test-tcpsock.lua
@@ -12,3 +12,23 @@ plan(29)
 
 -- 1. Success connection.
 local tcpsock, err = socket.tcp()
+local ok, err = tcpsock:connect("yechengfu.com", 80)
+is(ok, true)
+is(err, nil)
+like(tcpsock, "<tcpsock: %d+>") -- __tostring
+like(tcpsock:fileno(), "%d+")
+tcpsock:close()
+
+-- 2. Errors
+is(socket.ERROR_TIMEOUT, "Operation timed out")
+is(socket.ERROR_CLOSED, "Connection closed")
+is(socket.ERROR_REFUSED, "Connection refused")
+
+-- 3. Error: socket.ERROR_REFUSED/socket.ERROR_CLOSED
+local tcpsock = socket.tcp()
+local ok, err = tcpsock:connect("127.0.0.1", 16787)
+is(ok, nil)
+is(err, socket.ERROR_REFUSED)
+local bytes, err = tcpsock:write("hello")
+is(bytes, nil)
+is(err, socket.ERROR_CLOSED)
