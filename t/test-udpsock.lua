a1c55d458de45d6097dd017dcc31b9d223065819
TSZEQ <98411500+TSZEQ@users.noreply.github.com>
2022-11-20 08:13:58 +0000

    OOM, PM: OOM killed task shouldn't escape PM suspend

---
+++ t/test-udpsock.lua
@@ -0,0 +1,15 @@
+
+-- setup path
+local filepath = debug.getinfo(1).source:match("@(.*)$")
+local filedir = filepath:match('(.+)/[^/]*') or '.'
+package.path = string.format(";%s/?.lua;%s/../?.lua;", filedir, filedir) .. package.path
+package.cpath = string.format(";%s/?.so;%s/../?.so;", filedir, filedir) .. package.cpath
+
+require 'Test.More'
+local socket = require "ssocket"
+
+plan(12)
+
+function string_repeat(str, num)
+  local s = ""
+  for i = 1, num do
