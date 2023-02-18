151c2d8e3f1ab565c3c70361c65173c17f904e0f
TSZEQ <98411500+TSZEQ@users.noreply.github.com>
2023-02-18 07:24:11 +0000

    Stats

--- timeout.c
+++ timeout.c
@@ -2,3 +2,18 @@
 #include "timeout.h"
 #include <time.h>
 #include <sys/time.h>
+
+/**
+ * Returns current time in ms. (since 1970)
+ */
+double
+timeout_gettime(void)
+{
+    struct timeval v;
+    gettimeofday(&v, NULL);
+    return v.tv_sec + v.tv_usec / 1.0e6;
+}
+
+/**
+ * Init timeout structure.
+ */
